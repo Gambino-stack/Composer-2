0402afe1f1c194eda5a3b1d47ecc4f643cf5b376
Gambino-stack <69907371+Gambino-stack@users.noreply.github.com>
2020-10-22 18:45:27 +0200

    Add files via upload

---
+++ autoload/public/index.php
@@ -0,0 +1,10 @@
+<?php
+
+require __DIR__ . '/../vendor/autoload.php';
+
+
+
+
+$parl = new \HelloWorld\SayHello();
+
+ echo $parl->world();
---
+++ autoload/vendor/composer/installed.php
@@ -0,0 +1,33 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'No version set (parsed as 1.0.0)',
+    'version' => '1.0.0.0',
+    'aliases' => 
+    array (
+    ),
+    'reference' => NULL,
+    'name' => 'macbookyohan/autoload',
+  ),
+  'versions' => 
+  array (
+    'ehime/hello-world' => 
+    array (
+      'pretty_version' => '1.0.5',
+      'version' => '1.0.5.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'b1c8cdd2c11272d8c5deec7816e51fa5374217c1',
+    ),
+    'macbookyohan/autoload' => 
+    array (
+      'pretty_version' => 'No version set (parsed as 1.0.0)',
+      'version' => '1.0.0.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => NULL,
+    ),
+  ),
+);
